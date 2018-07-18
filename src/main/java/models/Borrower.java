db6d3d9db26690a8d12a201659c7aeb56e09988a
Gary Muir <garymuir17@gmail.com>
2018-07-18 13:57:03 +0100

    adds getId

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,11 +1,55 @@
 package models;
 
+import behaviours.IDB;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
-public class Borrower {
+public class Borrower implements IDB {
 
     private int id;
     private String name;
     private int libraryNumber;
-    private Set<Book> itemsBorrowed;
+    private List<Book> itemsBorrowed;
+
+    public Borrower() {}
+
+    public Borrower(String name, int libraryNumber) {
+        this.name = name;
+        this.libraryNumber = libraryNumber;
+        this.itemsBorrowed = new ArrayList<Book>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLibraryNumber() {
+        return libraryNumber;
+    }
+
+    public void setLibraryNumber(int libraryNumber) {
+        this.libraryNumber = libraryNumber;
+    }
+
+    public List<Book> getItemsBorrowed() {
+        return itemsBorrowed;
+    }
+
+    public void setItemsBorrowed(List<Book> itemsBorrowed) {
+        this.itemsBorrowed = itemsBorrowed;
+    }
 }
--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,5 +1,20 @@
+import models.Borrower;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
 
+    Borrower borrower;
 
+    @Before
+    public void before() {
+        this.borrower = new Borrower("Stuart", 0001);
+    }
 
+    @Test
+    public void hasId() {
+        assertEquals(0, borrower.getId());
+    }
 }
