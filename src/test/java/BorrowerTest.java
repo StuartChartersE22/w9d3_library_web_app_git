104340960230eb63c96d93d0c88da8f830e2953f
Gary Muir <garymuir17@gmail.com>
2018-07-18 14:02:30 +0100

    adds getters and setters for borrower class

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -23,4 +23,31 @@ public class BorrowerTest {
         borrower.setId(25);
         assertEquals(25, borrower.getId());
     }
+
+    @Test
+    public void hasName() {
+        assertEquals("Stuart", borrower.getName());
+    }
+
+    @Test
+    public void canSetName() {
+        borrower.setName("Gary");
+        assertEquals("Gary", borrower.getName());
+    }
+
+    @Test
+    public void hasLibraryNumber() {
+        assertEquals(0001, borrower.getLibraryNumber());
+    }
+
+    @Test
+    public void canSetLibraryNumber() {
+        borrower.setLibraryNumber(17);
+        assertEquals(17, borrower.getLibraryNumber());
+    }
+
+    @Test
+    public void itemsBorrowedStartsEmpty() {
+        assertEquals(0, borrower.getItemsBorrowed().size());
+    }
 }
